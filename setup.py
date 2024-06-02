38808ac967d7347695819ce05f71810ad4d8c040
Tushar Saini <tusharsaini1854@gmail.com>
2024-06-02 21:42:30 +0530

    [UPD] update cicd

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='pypi_cicd',
-    version='0.2',
+    version='0.3',
     long_description=read("README.md"),
     long_description_content_type='text/markdown',
     packages=find_packages(),
